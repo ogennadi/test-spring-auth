b736323c9f444f2ab4cd42d9aa42b1d67eebeea6
Oge Nnadi <oge@nnadi.org>
2022-09-25 20:00:22 -0500

    Document the app

--- src/main/java/com/example/testspringauth/WebController.java
+++ src/main/java/com/example/testspringauth/WebController.java
@@ -5,13 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
-//
-//    @GetMapping("/admin")
-//    String admin() {
-//        return "Hello, Admin";
-//    }
-
-
     @GetMapping("/")
     String index()    {
         return "Hello, Church people";
